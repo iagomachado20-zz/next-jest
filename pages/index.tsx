31f130dcd0af1ba45b03fb5ec6caa62020ba4eb7
Iago Machado da Silva <iago.silva@MacBook-Pro-de-Invillia.local>
2021-05-26 17:10:11 -0300

    chore: create link

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.scss'
+import Link from 'next/link';
 
 export default function Home() {
   return (
@@ -13,7 +14,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Learn <a href="https://nextjs.org">Next.js!</a>
+          <Link href="/auth/login">Login</Link>
         </h1>
 
         <p className={styles.description}>
